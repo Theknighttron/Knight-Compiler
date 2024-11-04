61319ab18153758bd7a7c190610df6d0c80acd66
polyhistor2050 <chrisspaul2050@gmail.com>
2024-11-04 11:55:32 +0300

    Main run func to load, decode and execute the VM

--- vm/vm.go
+++ vm/vm.go
@@ -1,6 +1,7 @@
 package vm
 
 import (
+	"fmt"
 	"knightcompiler/code"
 	"knightcompiler/compiler"
 	"knightcompiler/object"
@@ -29,9 +30,43 @@ func New(bytecode *compiler.Bytecode) *VM {
 }
 
 func (vm *VM) StackTop() object.Object {
+    // check if the stack if empty
     if vm.sp == 0 {
         return nil
     }
 
+    // return the top element in the stack
     return vm.stack[vm.sp-1]
+}
+
+func (vm *VM) Run() error {
+    // fetch the instruction
+    for ip := 0; ip < len(vm.instructions); ip++ {  // iterate through the instructions pointer
+        op := code.Opcode(vm.instructions[ip])      // turn the byte into an opcode
+
+        // decode the operands in the bytecode
+        switch op {
+        case code.OpConstant:
+            constIndex := code.ReadUint16(vm.instructions[ip+1:])
+            ip +=2
+            err := vm.push(vm.constants[constIndex])    // push the constants onto the stack
+            if err != nil {
+                return err
+            }
+        }
+    }
+
+    return nil
+}
+
+func (vm *VM) push(o object.Object) error {
+    if vm.sp >= StackSize {
+        return fmt.Errorf("stack overflow")
+    }
+
+    vm.stack[vm.sp] = o
+    vm.sp++
+
+
+    return nil
 }
